9f031f5b9a38729b7c6dea9e7d13573f587004e8
jwu8483 <43213089+jwu8483@users.noreply.github.com>
2018-09-23 22:19:20 -0400

    Update jwulib.java

--- src/JackieWu/jwulib.java
+++ src/JackieWu/jwulib.java
@@ -1,38 +1,73 @@
 package JackieWu;
-public class jwulib {
+public class jWuLib {
+    //String Methods
     public static boolean isPalindrome(String str) {
 
         String x = "";
-        for (int i = str.length(); i > 0; i--) {
+        for (int i = str.length(); i > 0; i--) {//Reverses the string
             x = x + str.substring(i - 1, i);
         }
-        if (str.equals(x)) {
+        if (str.equals(x)) {//Checks if the reversed string is equal to the original
             return true;
         } else {
             return false;
         }
     }
-        public static String dateStr(String str){
 
-            String Month = str.substring(0,2);
-            String day = str.substring(3,5);
-            String Year =str.substring(6,10);
-            return day + "-" + Month + "-" + Year;
-        }
-        public static int isFibonnaci(int n) {
+    public static String dateStr(String str) {
 
-            while (int i < n;)
-            {
-
-
-            }
-        }
-
-
-    public static void main(String[] args) {
-        System.out.println(isPalindrome("dog"));
-        System.out.println(dateStr("12/13/1212"));
+        String Month = str.substring(0, 2);//Takes the Month part of the string
+        String day = str.substring(3, 5);//Takes the Day part of the string
+        String Year = str.substring(6, 10);//Takes the year part of the string
+        return day + "-" + Month + "-" + Year;//Rearranges the string and adds "-" in between
     }
 
-}
+    public static void fooBarBaz(int n)
+    {
+        for (int i = 1; i < n + 1; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("baz");//Prints "baz" when it is a multiple of 5 and 3
+            }
+            else if (i % 3 == 0) {
+                System.out.println("foo");//Prints "foo" when it is a multiple of 3
+            }
+            else if (i % 5 == 0) {
+                System.out.println("bar");//Prints "bar" when it is a multiple of 5
+            }
+            else
+            {
+                System.out.println(i);//Prints out the number if it isn't a multiple of 3 or 5
+            }
 
+        }
+    }
+    //Math Methods
+    public static int sumUpTo(int n) {
+        int x = 0;
+        for (int i = 0; i < n + 1; i++) {//Adds the number and previous number until the nth number
+            x = x + i;
+        }
+        return x;
+    }
+    public static void primePrinter(int num) {
+        int n = 0;
+        String result = "";
+        int i = 2;
+        int k = 0;
+        while (n < num) {
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    k++;
+                }
+            }
+            if (k == 2) {//Checks if number has only 2 factors
+                result += i + " ";
+                n++;
+            }
+            i++;
+            k = 0;
+
+        }
+            System.out.println(result);
+        }
+    }
